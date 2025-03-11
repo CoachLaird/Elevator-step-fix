fb7417b53877a51ae3da443393ec336184e15f64
CoachLaird <lairdt@covertps.org>
2025-03-11 08:50:55 -0400

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -125,11 +125,17 @@ public class RobotContainer {
                 .onFalse(new InstantCommand(
                         () -> m_endeffector.StopEndeffector()));
 
-        // Add D-Pad controls for elevator
-        joystickOperator.povUp()
-                .onTrue(new InstantCommand(() -> m_elevatorSubsystem.stepUp()));
-        joystickOperator.povDown()
-                .onTrue(new InstantCommand(() -> m_elevatorSubsystem.stepDown()));
+        // Add joystick controls for elevator
+       drivetrain.setDefaultCommand(
+        drivetrain.applyRequest(() -> {
+            double rightY = joystickOperator.getRightY();
+            if (rightY > 0.5) {
+                m_elevatorSubsystem.ElevatorToSetpoint(m_elevatorSubsystem.getCurrentPosition() + 1);
+            } else if (rightY < -0.5) {
+                m_elevatorSubsystem.ElevatorToSetpoint(m_elevatorSubsystem.getCurrentPosition() - 1);
+            }
+        })
+    );
         
                 }
 
